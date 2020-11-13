c3a38cafc98b2cf88e934847acf0d11bbe2bba30
GauravDarkslayer <www.gaurav10bhojwani@gmail.com>
2020-11-13 21:29:36 +0530

    combined function

--- test_gen_webapp/test_gen/views.py
+++ test_gen_webapp/test_gen/views.py
@@ -107,7 +107,81 @@ def preview(request):
                 cam_micro = cam_micro,
                 test_duration = test_duration
             )
-            context = {
+
+            file = request.FILES["mydocument"]
+            # print("this is file object",file)
+            # current_user_email = request.session["email"]
+            import docx
+            try:
+                doc = docx.Document('file.docx')
+                print("success")
+            except Exception as e:
+                print(e)
+            
+            doc = docx.Document(file)
+            print("this works")
+            questionnaire={}
+            explanation={}
+            diagram={}
+            fill_blanks = {}
+            i=1
+            number_of_iterations = 0
+            for content in doc.paragraphs:
+                if ("Q"+str(i) or "QUESTION"+str(i)) in content.text.upper():
+                    print(content.text)
+                    # questionnaire["question"+str(i)] = content.text
+                    questionnaire[content.text] =[]
+                    i=i+1
+                    j=1
+                    for option in doc.paragraphs:
+                        # print("this is option",option.text.upper())
+                        if "OPTION"+" "+str(j) in option.text.upper():
+                            print("check")
+                            questionnaire[content.text].append(option.text)
+                            j= j+1
+                        if "ANSWER" in option.text.upper():
+                            print("get answer")
+                            questionnaire[content.text].append(option.text)
+                            break 
+                elif ('fill in the blanks') in content.text.lower():
+                    print("I am inside Fill in the Blanks")
+                    fill_blanks[content.text] = []
+                    next_index = number_of_iterations+1
+                    
+                    for i in range(next_index, len(doc.paragraphs)):
+                        if "_" in doc.paragraphs[i].text:
+                            fill_blanks[content.text].append(doc.paragraphs[i].text)
+                        elif " " == doc.paragraphs[i].text:
+                            continue
+                        else:
+                            break
+                elif("explain") in content.text.lower():
+                    explanation[content.text]=[]
+                    next_index = number_of_iterations+1
+                    for i in range(next_index, len(doc.paragraphs)):
+                        if "Write" in doc.paragraphs[i].text:
+                            explanation[content.text].append(doc.paragraphs[i].text)
+                        elif " " == doc.paragraphs[i].text:
+                            continue
+                        else:
+                            break
+                elif("draw") in content.text.lower():
+                    diagram[content.text]=[]
+                    next_index = number_of_iterations+1
+                    for i in range(next_index, len(doc.paragraphs)):
+                        if "diagram" in doc.paragraphs[i].text:
+                            diagram[content.text].append(doc.paragraphs[i].text)
+                        elif " " == doc.paragraphs[i].text:
+                            continue
+                        else:
+                            break
+
+                    print("Explanation",explanation)
+                    print("Diagram",diagram)
+                    print("Data of Fill in the blanks is ", fill_blanks)
+                number_of_iterations += 1
+            print(questionnaire)
+            preview_data = {
                 "name":name,
                 "description":description,
                 "logo":logo,
@@ -117,7 +191,7 @@ def preview(request):
                 "test_duration":test_duration
             }
             messages.success(request,"Test created successfully")
-            return render(request,"form.html",context=context)
+            return render(request,"form.html",{'preview_data':preview_data,'context':questionnaire, 'fill_blanks':fill_blanks,"explain":explanation,"diagram":diagram})
         except Exception as e:
             messages.error(request,e)
             return render(request,"form.html")
